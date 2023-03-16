8daac3c4b61e6f041451be3b358a9b0149fb3fdf
yakine benali <yakinebenali5@gmail.com>
2023-03-16 12:50:28 +0100

    feat"add router allafiche" index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { RegisterEtu,LoginEtu} from "../controllers/Etudiants.js";
+import { RegisterEtu,LoginEtu, getAllAffiches} from "../controllers/Etudiants.js";
+import { verifyToken } from "../middelware/VerifyToken.js";
 
 const router = express.Router();
 router.post('/LoginEtu', LoginEtu);
-
 router.post('/RegisterEtu', RegisterEtu);
+router.get('/allaffiche',verifyToken,getAllAffiches);
+
 
 export default router;
