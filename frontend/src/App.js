9667234c26597f7d9ea50a71d7416f6b32ebdf65
yakine benali <yakinebenali5@gmail.com>
2023-03-30 02:09:45 +0100

    feat :p

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
+
 import React from "react";
+import Home from "./components/Home";
 import Login from "./components/Login";
-import './App.css';
+import Signup from "./components/Singup";
+import { Fragment } from "react";
+import { BrowserRouter,Route,Redirect} from "react-router-dom";
+import './App.css'
+
+
+
+
 
 function App() {
-  return (
+	const user = localStorage.getItem("token");
+	return (
     
-     <Login></Login>
-   
-  );
+    <Fragment>
+      <BrowserRouter>
+      <Route  path="/signup" ><Signup/></Route>
+		 	<Route path="/login" ><Login/></Route>
+       {user && <Route path="/" ><Home/></Route>} 
+			{! user && <Route  exact path="/" > <Redirect  to="/login" /></Route>}
+      </BrowserRouter>
+    </Fragment>
+
+	);
 }
 
 export default App;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,33 +1,73 @@
-import React from "react";
-import './Login.css';
+import { useState } from "react";
+import axios from "axios";
+
+import "./login.css";
+
+
 const Login = () => {
-    return (
+	const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [msg, setMsg] = useState('');
+
+    const Auth = async (e) => {
+        e.preventDefault();
+        try {
+		const { data: res } = await axios.post('http://localhost:3000/LoginEtu', {
+					
+		email: email,
+		password: password
+	});
+				localStorage.setItem("token",res.data);
+				 
+				
+				window.location = "/Home";
+        } catch (error) {
+			if (error.response)   {
+                setMsg(error.response.data.msg);
+            }
+        }
+    }
+
+
+	return (
 		
 		<div className="login_container">
 			<div className="login_form_container">
 				<div className="right">
-						<h1 id="a">Log In</h1>
-                        <form className="form_container">
+					<form  onSubmit={Auth}  className="form_container" >
+						<h1>Login to Your Account</h1>
 						<input
 							type="email"
 							placeholder="Email"
 							name="email"
+							value={email} 
+							onChange={(e) => setEmail(e.target.value)}
+							required
 							className="input"
 						/>
-                        <br></br>
 						<input
 							type="password"
 							placeholder="Password"
 							name="password"
+							value={password} 
+							onChange={(e) => setPassword(e.target.value)}
+							required
 							className="input"
 						/>
-                        <br></br>
-						<button type="submit" className="red_btn">
-							connect
+						{msg && <div className="error_msg">{msg}</div>}
+						<button type="submit" className="green_btn">
+							Sing In
 						</button>
 					</form>
 				</div>
-				
+				<div className="left ">
+					<h1>New Here ?</h1>
+					<a href="/signup">
+						<button type="button" className="white_btn">
+							Sing Up
+						</button>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
