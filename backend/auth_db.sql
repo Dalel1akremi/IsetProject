f3d1638930f0c1fab3db19abbee4598130af2234
yakinebenali <yakinebenali5@gmail.com>
2023-04-02 14:33:14 +0100

    feat :chore clean up "auth-db "

--- backend/auth_db.sql
+++ backend/auth_db.sql
@@ -29,10 +29,7 @@ ALTER TABLE `etudiants`
 COMMIT;
 
 -----table de chef departement ----
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+
 
 CREATE TABLE chef_departement(
    id VARCHAR(50),
@@ -48,10 +45,9 @@ CREATE TABLE chef_departement(
    nom_dep VARCHAR(50) NOT NULL,
    PRIMARY KEY(id)
 );
+-- AUTO_INCREMENT for table `chef_departement`--
 ALTER TABLE `chef_departement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-=======
->>>>>>> a01a598183f4b8a764ce43691938dbbb0e50fd90
 COMMIT;
 CREATE TABLE admin(
    id VARCHAR(50),
@@ -73,3 +69,4 @@ CREATE TABLE admin(
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
+
