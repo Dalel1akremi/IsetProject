e2c0b50511a8c6c366d0e0d06f7410a157ad1f74
Dalel1akremi <dalelakremi2020@gmail.com>
2023-04-02 15:57:08 +0100

    feat: add "RegisterAdmin" and "LoginAdmin" routers in "routes" js file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import { RegisterEtu,LoginEtu, getAllAffiches} from "../controllers/Etudiants.js";
 import { verifyToken } from "../middelware/VerifyToken.js";
 import { RegisterChef, LoginChef } from "../controllers/ChefDep.js";
+import { RegisterAdmin, LoginAdmin } from "../controllers/Admin.js";
 const router = express.Router();
 router.post('/LoginEtu', LoginEtu);
 router.post('/RegisterEtu', RegisterEtu);
 router.get('/allaffiche',verifyToken,getAllAffiches);
 router.post('/RegisterChef', RegisterChef);
 router.post('/LoginChef',LoginChef);
+router.post('/RegisterAdmin', RegisterAdmin);
+router.post('/LoginAdmin',LoginAdmin);
 
 export default router;
